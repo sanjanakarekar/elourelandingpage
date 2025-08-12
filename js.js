d0e5f39e880cee5354dfa6f103cec0ffe4db74d8
sanjanakarekar <130066415+sanjanakarekar@users.noreply.github.com>
2025-08-12 23:29:27 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', () => {
+  const about = document.querySelector('.about-text');
+  const aboutPos = about.getBoundingClientRect().top;
+  const screenPos = window.innerHeight / 1.2;
+
+  if (aboutPos < screenPos) {
+    about.classList.add('fade-in');
+  }
+});
